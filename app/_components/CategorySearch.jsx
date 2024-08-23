3b1f7822854bda12107c9f359055219f4a630d4f
Rohith Goli <rohithgoli396@gmail.com>
2024-08-23 22:00:55 +0530

    feat: added search bar with category list display

---
+++ app/_components/CategorySearch.jsx
@@ -0,0 +1,58 @@
+"use client"
+import React, {useState, useEffect} from 'react'
+
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { Search } from 'lucide-react'
+import GlobalApi from '../_utils/GlobalApi'
+
+const CategorySearch = () => {
+
+    const [categoryList, setCategoryList] = useState([])
+
+    useEffect(() => {
+        getCategoryList()
+    }, [])
+
+    const getCategoryList = () => {
+        GlobalApi.getCategory().then(resp => {
+            console.log(resp.data.data);
+            setCategoryList(resp.data.data)
+        })
+    }
+
+  return (
+    <div className='mb-10 items-center px-5 flex flex-col gap-4'>
+        <h2 className='font-bold text-4xl tracking-wide'>
+            Search <span className='text-primary'>Doctors</span>
+        </h2>
+        <h2 className='text-gray-500 text-xl'>Search Your Doctor and Book Appointment in one click</h2>
+        <div className="flex w-full max-w-sm items-center space-x-2 mt-3">
+            <Input type="search" placeholder="Search..." />
+            <Button type="submit">
+                <Search className='h-4 w-4 mr-2'/>
+                Search
+            </Button>
+        </div>
+
+        # Display List of Category
+        <div className='grid grid-cols-3 mt-5 md:grid-cols-4 lg:grid-cols-6'>
+            {categoryList.map((item, index) => index<6 && (
+                <div 
+                    key={index}
+                    className='flex flex-col text-center items-center p-5
+                     bg-blue-50 gap-2 m-2 rounded-lg hover:scale-110 transition-all ease-in-out cursor-pointer'>
+                        <Image src={item.attributes?.Icon?.data.attributes?.url}
+                        alt='icon'
+                        width={40}
+                        height={40}
+                        />
+                        <label className='text-blue-600 text-sm'>{item?.attributes?.Name}</label>
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default CategorySearch
---
+++ app/_utils/GlobalApi.jsx
@@ -0,0 +1,15 @@
+const {axios} = require("axios");
+
+const API_KEY = process.env.NEXT_PUBLIC_STRAPI_API_KEY
+
+const axiosClient = axios.create({
+    baseURL: 'http://localhost:1337/api',
+    headers: {
+        'Authorization': `Bearer ${API_KEY}`
+    }
+})
+
+const getCategory = () => axiosClient.get('categories?populate=*')
+export default {
+    getCategory
+}
