8c45388ae9b210c3b9256e5411bd818340263298
Rohith Goli <rohithgoli396@gmail.com>
2024-08-25 16:11:23 +0530

    feat: added simple routing, dynamic routing, accessing the dynamic value from route using params

---
+++ app/(route)/search/[cname]/page.js
@@ -0,0 +1,15 @@
+"use client"
+import React, { useEffect } from 'react'
+
+const Search = ({params}) => {
+  
+  useEffect(() => {
+    console.log(params)
+  }, [])
+
+  return (
+    <div>Search</div>
+  )
+}
+
+export default Search
